92038ba0b8255fe09ab87e6850146dd367321b50
elicas17 <73227850J@CEBANCDEA.EDU>
2021-02-01 20:28:56 +0100

    c-produ

---
+++ js/index.js
@@ -0,0 +1,148 @@
+window.onload = () => {
+	pedido="";
+	
+	datosPedido=[];
+
+	cargarBocadillos();
+	cargarCarro();
+	document.getElementById("menuBoca").addEventListener('click',cargarBocadillos);
+	document.getElementById("menuBebi").addEventListener('click',cargarBebidas);
+	// anadir()
+
+}
+
+function cargarBebidas(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('GET', 'bebidas.json', true);
+	xhttp.send();
+	xhttp.onreadystatechange= function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let datos=JSON.parse(this.responseText);
+			
+			let res=document.querySelector("#c-productos");
+			res.innerHTML = ' ';
+			for(let item of datos){
+				res.innerHTML+=`
+					<div class="c-produc">
+						<div>
+							<img src="images/${item.src}" width="200">
+						</div>
+						<div>
+							<p>${item.nombre}</p>
+							<p>${item.precio}</p>
+							<div>
+								<input type="radio" id="contactChoice1"
+								name="contact" value="email">
+								<label for="contactChoice1">Frio</label>
+						
+								<input type="radio" id="contactChoice2"
+								name="contact" value="phone">
+								<label for="contactChoice2">Templado</label>
+							</div>
+							
+						</div>
+						<div>
+							<button id="quitar-bebi-${item.id}">Quitar</button>
+							<p id="cantidad-bebi-${item.id}">0</p>
+							<button id="anadir-bebi-${item.id}">Añadir</button>
+						</div>
+					</div>	
+				`
+			}
+		  }
+	};
+}
+function cargarBocadillos() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('GET', 'bocadillos.json', true);
+	xhttp.send();
+	xhttp.onreadystatechange= function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let datos=JSON.parse(this.responseText);
+			
+			let res=document.querySelector("#c-productos");
+			res.innerHTML = '';
+			for(let item of datos){
+				res.innerHTML+=`
+					<div class="c-produc">
+						<div>
+							<img src="images/${item.src}" width="200">
+						</div>
+						<div>
+							<p>${item.nombre}</p>
+							<p>${item.precio}</p>
+							<div>
+								<ul id="c-ingre-${item.id}"></ul>
+							</div>
+						</div>
+						<div>
+							<button id="quitar-boca-${item.id}">Quitar</button>
+							<p id="cantidad-boca-${item.id}">0</p>
+							<button id="anadir-boca-${item.id}">Añadir</button>
+						</div>
+					</div>	
+				`
+				var conte_ingre=document.getElementById(`c-ingre-${item.id}`);
+
+				for(let item2 of item.ingredientes)
+				conte_ingre.innerHTML+=`
+					<li> ${item2}</li>
+				`
+			}
+			anadir();
+			
+		  }
+	};
+}
+function cargarCarro(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('GET', 'bocadillos.json', true);
+	xhttp.send();
+	xhttp.onreadystatechange= function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let datos=JSON.parse(this.responseText);
+			
+			let res=document.querySelector("#tbody");
+			res.innerHTML += '';
+			for(let item of datos){
+				res.innerHTML+=`
+					<tr>
+						<td>${item.nombre}</td>
+						<td>0</td>
+						<td>${item.precio}</td>
+					</tr>
+				`
+			}
+
+		}
+	};
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('GET', 'bebidas.json', true);
+	xhttp.send();
+	xhttp.onreadystatechange= function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let datos=JSON.parse(this.responseText);
+			
+			let res=document.querySelector("#tbody");
+			res.innerHTML += '';
+			for(let item of datos){
+				res.innerHTML+=`
+					<tr>
+						<td>${item.nombre}</td>
+						<td>0</td>
+						<td>${item.precio}</td>
+					</tr>
+				`
+			}
+
+		}
+	}
+}
+function anadir(){
+	console.log(document.getElementById("cantidad-boca-1").innerHTML);
+	document.getElementById("anadir-boca-1").addEventListener('click',boca1=()=>{
+		cant_boca_1= parseInt(document.getElementById("cantidad-boca-1").innerHTML)+1 ;
+		document.getElementById("cantidad-boca-1").innerHTML=cant_boca_1;
+
+	})
+}
